7852ae468f5c762143490a76b9bed6fd2813ba2c
MandyXue <13564039693@163.com>
2016-06-12 20:11:39 +0800

    schedule page: first page finished

--- Traveller/Traveller/TimelineTableViewController.swift
+++ Traveller/Traveller/TimelineTableViewController.swift
@@ -10,11 +10,12 @@ import UIKit
 
 class TimelineTableViewController: UITableViewController {
     
+    var cities: [String] = []
+    
     // MARK: - BaseViewController
     
     static func loadFromStoryboard() -> UIViewController {
         let controller = UIStoryboard.traveller_scheduleStoryboard().instantiateViewControllerWithIdentifier(self.traveller_className())
-//        let navigationController: UINavigationController = UINavigationController.init(rootViewController: controller)
         return controller
     }
     
@@ -25,16 +26,18 @@ class TimelineTableViewController: UITableViewController {
 
         // delete selection between presentations
         self.clearsSelectionOnViewWillAppear = true
-
-        // display an Edit button in the navigation bar for this view controller.
         
+        prepareData()
+        
+        let longPress = UILongPressGestureRecognizer(target: self, action: #selector(longPressGestureRecognized))
+        self.tableView.addGestureRecognizer(longPress)
     }
     
     override func viewWillAppear(animated: Bool) {
         // set up navigation bar
         self.tabBarController?.navigationItem.title = "Schedule"
-        self.tabBarController?.navigationItem.rightBarButtonItem = self.editButtonItem()
-        self.tabBarController?.navigationItem.leftBarButtonItem = nil
+        self.tabBarController?.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: #selector(addDestination))
+        self.tabBarController?.navigationItem.leftBarButtonItem = self.editButtonItem()
     }
 
     override func didReceiveMemoryWarning() {
@@ -44,69 +47,136 @@ class TimelineTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return cities.count
     }
-
-    /*
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCellWithIdentifier("DestinationCell", forIndexPath: indexPath) as! DestinationTableViewCell
+        cell.destImageView?.image = UIImage(named: "testPlace")
+        cell.destNameLabel.text = cities[indexPath.row]
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
-
-    /*
-    // Override to support editing the table view.
+    
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
             // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            if indexPath.row < cities.count {
+                cities.removeAtIndex(indexPath.row)
+                tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Left)
+            }
+        }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
+    
+    // MARK: - Helper
+    
+    func prepareData() {
+        cities.append("Shanghai, China")
+        cities.append("Uppsala, Sweden")
+        cities.append("Paris, France")
+        cities.append("Guangzhou, China")
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
+    
+    func addDestination() {
+        // TODO: 进入地图选点
+        print("add")
+        cities.append("New")
+        tableView.reloadData()
     }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    
+    var snapshot: UIView? = nil
+    var sourceIndexPath: NSIndexPath? = nil
+    
+    func longPressGestureRecognized(sender: AnyObject?) {
+        if let longPress = sender as? UILongPressGestureRecognizer {
+            let state = longPress.state
+            
+            let location = longPress.locationInView(self.tableView)
+            if let indexPath = self.tableView.indexPathForRowAtPoint(location) {
+                
+                
+                
+                switch state {
+                case .Began:
+                    sourceIndexPath = indexPath
+                    print(indexPath.row)
+                    if let cell = tableView.cellForRowAtIndexPath(indexPath) {
+                        // take a snapshot of the selected row using helper method
+                        snapshot = self.customSnapshotFromView(cell)
+                        
+                        var center = cell.center
+                        snapshot!.center = center
+                        snapshot!.alpha = 0.0
+                        self.tableView.addSubview(snapshot!)
+                        UIView.animateWithDuration(0.25, animations: {
+                            center.y = location.y
+                            self.snapshot!.center = center
+                            self.snapshot!.transform = CGAffineTransformMakeScale(1.05, 1.05)
+                            self.snapshot!.alpha = 0.98
+                            cell.alpha = 0.0
+                            cell.hidden = true
+                        })
+                    }
+                    break
+                case .Changed:
+                    var center = snapshot!.center
+                    center.y = location.y
+                    snapshot!.center = center
+                    if !indexPath.isEqual(sourceIndexPath) {
+                        let temp = NSMutableArray(array: cities)
+                        temp.exchangeObjectAtIndex(indexPath.row, withObjectAtIndex: sourceIndexPath!.row)
+                        self.cities = temp as AnyObject as! [String]
+                        // TODO: 将cities存到远端
+                        self.tableView.moveRowAtIndexPath(sourceIndexPath!, toIndexPath: indexPath)
+                        
+                        sourceIndexPath = indexPath
+                    }
+                    break
+                default:
+                    if let cell = self.tableView.cellForRowAtIndexPath(sourceIndexPath!) {
+                        cell.alpha = 0.0
+                        UIView.animateWithDuration(0.25, animations: {
+                            self.snapshot!.center = cell.center
+                            self.snapshot!.transform = CGAffineTransformIdentity
+                            self.snapshot!.alpha = 0.0
+                            cell.alpha = 1.0
+                            }, completion: { (finished: Bool) in
+                                cell.hidden = false
+                                self.sourceIndexPath = NSIndexPath(forRow: 0, inSection: 0)
+                                self.snapshot!.removeFromSuperview()
+                                self.snapshot = UIImageView()
+                        })
+                    }
+                    break
+                }
+            }
+            
+            
+        }
     }
-    */
+    
+    func customSnapshotFromView(inputView: UIView) -> UIView {
+        UIGraphicsBeginImageContextWithOptions(inputView.bounds.size, false, 0)
+        inputView.layer.renderInContext(UIGraphicsGetCurrentContext()!)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        // create an image view
+        let snapshot = UIImageView(image: image)
+        snapshot.layer.masksToBounds = false
+        snapshot.layer.cornerRadius = 0.0
+        snapshot.layer.shadowOffset = CGSizeMake(-5.0, 0.0)
+        snapshot.layer.shadowRadius = 5.0
+        snapshot.layer.shadowOpacity = 0.4
+        
+        return snapshot
+    }
 
 }
