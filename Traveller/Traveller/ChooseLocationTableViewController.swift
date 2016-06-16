715402f251387ac2f3ff7002bcc7c6842fa673e0
Teng <843018739@qq.com>
2016-06-16 17:28:17 +0800

    completed map search

--- Traveller/Traveller/ChooseLocationTableViewController.swift
+++ Traveller/Traveller/ChooseLocationTableViewController.swift
@@ -4,13 +4,21 @@
 //
 //  Created by MandyXue on 16/6/12.
 //  Copyright © 2016年 AppleClub. All rights reserved.
-//
+//  地图搜索结果
 
 import UIKit
+import MapKit
+
+protocol MapSearchDelegate {
+    func setSearchResult(selectedLocaiton:MKMapItem)
+}
 
 class ChooseLocationTableViewController: UITableViewController {
     
     // MARK: - BaseViewController
+    
+    var searchResult = [MKMapItem]()
+    var resultDelegate:MapSearchDelegate?
     
     static func loadFromStoryboard() -> UIViewController {
         let controller = UIStoryboard.traveller_chooseLocationStoryboard().instantiateViewControllerWithIdentifier(self.traveller_className())
@@ -32,30 +40,39 @@ class ChooseLocationTableViewController: UITableViewController {
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return searchResult.count
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+        let cell = tableView.dequeueReusableCellWithIdentifier("searchResultCell", forIndexPath: indexPath)
 
-        // Configure the cell...
+        let titleLabel = cell.viewWithTag(101) as! UILabel
+        titleLabel.text = searchResult[indexPath.row].name
+        
+        let detailLabel = cell.viewWithTag(102) as! UILabel
+        detailLabel.text = searchResult[indexPath.row].placemark.thoroughfare
 
         return cell
     }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        // 数据回传
+        self.resultDelegate?.setSearchResult(searchResult[indexPath.row])
+        
+        self.navigationController?.popViewControllerAnimated(true)
+    }
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+//    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+//        
+//    }
 
 }
--- Traveller/Traveller/ChooseLocationViewController.swift
+++ Traveller/Traveller/ChooseLocationViewController.swift
@@ -4,15 +4,17 @@
 //
 //  Created by MandyXue on 16/6/12.
 //  Copyright © 2016年 AppleClub. All rights reserved.
-//
+//  地图选点页面
 
 import UIKit
 import MapKit
 
-class ChooseLocationViewController: UIViewController, UISearchBarDelegate {
+class ChooseLocationViewController: UIViewController, UISearchBarDelegate, MapSearchDelegate {
     
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var mapView: MKMapView!
+    
+    var selectedPoint:MKMapItem?
     
     // MARK: - BaseViewController
     
@@ -29,6 +31,29 @@ class ChooseLocationViewController: UIViewController, UISearchBarDelegate {
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Done", style: .Done, target: self, action: #selector(makeChoice))
         self.searchBar.delegate = self
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if let _ = selectedPoint {
+            // 放置selectedPoint大头针
+            let selectedAnnotation = MKPointAnnotation()
+            selectedAnnotation.coordinate = selectedPoint!.placemark.coordinate
+            selectedAnnotation.title = selectedPoint!.placemark.title
+            selectedAnnotation.subtitle = selectedPoint!.placemark.thoroughfare
+            
+            mapView.addAnnotation(selectedAnnotation)
+            mapView.centerCoordinate = selectedAnnotation.coordinate
+            
+            let latDelta = 0.05
+            let longDelta = 0.05
+            let currentLocationSpan:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, longDelta)
+
+            
+            let currentRegion = MKCoordinateRegion(center: selectedAnnotation.coordinate, span: currentLocationSpan)
+            mapView.setRegion(currentRegion, animated: true)
+        }
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -39,14 +64,59 @@ class ChooseLocationViewController: UIViewController, UISearchBarDelegate {
     
     func makeChoice() {
         print("make choice")
+        // 把地图选点结果回传上个页面
+        
+        if let _ = self.selectedPoint {
+            // 回传数据
+            
+            self.navigationController?.popViewControllerAnimated(true)
+        }
     }
+    
+    func setSearchResult(selectedLocaiton: MKMapItem) {
+        self.selectedPoint = selectedLocaiton
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        print(searchBar.text)
+        
+        // 根据输入搜索地点
+        let request = MKLocalSearchRequest()
+        request.naturalLanguageQuery = searchBar.text!
+        request.region = mapView.region
+        let search = MKLocalSearch(request: request)
+        
+        search.startWithCompletionHandler { (response, error) in
+            guard let response = response else {
+                // TODO: 添加错误处理
+                print("Search error: \(error)")
+                return
+            }
+            
+            for item in response.mapItems {
+                print("search result:\(item.name)")
+                print("content:\(item.description)")
+            }
+            
+            let searchResult = response.mapItems
+            
+            let viewController = self.storyboard?.instantiateViewControllerWithIdentifier("ChooseLocationTableViewController") as! ChooseLocationTableViewController
+            
+            viewController.searchResult = searchResult
+            viewController.resultDelegate = self
+            searchBar.resignFirstResponder()
+            self.navigationController?.pushViewController(viewController, animated: true)
+        }
+    }
+    
+    
     
     // MARK: - Navigation
     
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+//    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
-    }
+//    }
 
 }
