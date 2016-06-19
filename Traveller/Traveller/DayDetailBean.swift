69068f5d58eb0ef49a9c328cc5207c4ef9623c5e
Teng <843018739@qq.com>
2016-06-19 16:25:46 +0800

    add DayDetailBean

---
+++ Traveller/Traveller/DayDetailBean.swift
@@ -0,0 +1,45 @@
+//
+//  DayDetailBean.swift
+//  Traveller
+//
+//  Created by Teng on 6/19/16.
+//  Copyright © 2016 AppleClub. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+class DayDetailBean: DataBean {
+    
+    var id: String?
+    var planID: String
+    var postID: String?
+    var startTime: NSDate
+    var endTime: NSDate
+    var place: String
+    var coordinate: CLLocationCoordinate2D
+    var type: Int
+    
+    // From remote
+    init (id: String, planID: String, postID:String?, startTime: NSDate, endTime: NSDate, place: String, latitude: Double, longitude: Double, type: Int) {
+        self.id = id
+        self.planID = planID
+        self.postID = postID
+        self.startTime = startTime
+        self.endTime = endTime
+        self.place = place
+        self.coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        self.type = type
+    }
+    
+    // From local
+    init (planID: String, postID: String?, startTime: NSDate, endTime: NSDate, place: String, latitude: Double, longitude: Double, type: Int) {
+        self.planID = planID
+        self.postID = postID
+        self.startTime = startTime
+        self.endTime = endTime
+        self.place = place
+        self.coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        self.type = type
+    }
+}
